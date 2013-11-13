799d7b8846eef0bbb90c5d6915696744aa8c69be
rendu <rendu@rendu-Lower.(none)>
2013-11-13 17:10:48 +0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    #@user = User.new(params[user_params])    # Not the final implementation!
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # Handle a successful save.
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
