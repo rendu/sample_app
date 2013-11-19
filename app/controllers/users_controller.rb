a9005828301740b2f73de7068a97c72d23f4e286
rendu <rendu@sina.com.cn>
2013-11-19 16:57:10 +0800

    Finish user edit,update,index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,40 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+    #@users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+  #  @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] =  "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -22,7 +52,24 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  # Before filters
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." #unless signed_in?
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
