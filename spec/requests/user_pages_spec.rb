4e3f114c9aaa7038d83ac94cd6c17faf38d80dc0
rendu <rendu@sina.com.cn>
2013-11-21 10:11:50 +0800

    add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -132,4 +132,21 @@ describe "UserPages" do
       specify { expect(user.reload.email).to eq new_email }
     end
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user)}
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo")}
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "bRa")}
+
+    before { visit user_path(user)}
+
+    it { should have_content(user.name)}
+    it { should have_title(user.name)}
+
+    describe "micropost" do
+      it { should have_content(m1.content)}
+      it { should have_content(m2.content)}
+      it { should have_content(user.microposts.count)}
+    end
+  end
 end
